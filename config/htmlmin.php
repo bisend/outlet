addef2da0328a78d75f04584d3e83e3d434eeeae
Coderonix <coderonix@gmail.com>
2018-06-05 00:09:19 +0300

    Added mail views to ignore section

--- config/htmlmin.php
+++ config/htmlmin.php
@@ -57,14 +57,8 @@ return [
     */
 
     'ignore' => [
-        'resources/views/emails',
-        'resources/views/html',
-        'resources/views/notifications',
-        'resources/views/markdown',
-        'resources/views/vendor/emails',
-        'resources/views/vendor/html',
-        'resources/views/vendor/notifications',
-        'resources/views/vendor/markdown',
+        'resources/views/email',
+        'resources/views/vendor',
     ],
 
 ];
